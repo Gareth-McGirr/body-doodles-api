b996e3fbe7a6561e6aadaa09457a231eb8b01e81
Gareth-McGirr <gareth.mcgirr@hotmail.com>
2022-09-23 15:21:47 +0100

    Fix import in profiles model

--- profiles/models.py
+++ profiles/models.py
@@ -1,4 +1,3 @@
-from asyncio.windows_events import NULL
 from django.db import models
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
@@ -11,7 +10,7 @@ class Profile(models.Model):
     name = models.CharField(max_length=255, blank=True)
     content = models.TextField(blank=True)
     image = models.ImageField(upload_to="profile_images/", default="profile_images/default_profile.jpg")
-    artistId = models.IntegerField(default=0)
+    artistId = models.IntegerField(null=True)
 
     class Meta:
         ordering = ["-created_at"]
