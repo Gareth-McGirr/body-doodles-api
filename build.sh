cc8eff2692821ef6f6a8f8dfc9d5ea72d00beaa8
Gareth-McGirr <gareth.mcgirr@hotmail.com>
2023-02-28 00:50:51 +0000

    update build.sh for render deploy

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 set -o errexit
- pip install -r requirements.txt
- python manage.py makemigrations && python manage.py migrate
+pip install -r requirements.txt
+python manage.py makemigrations && python manage.py migrate
