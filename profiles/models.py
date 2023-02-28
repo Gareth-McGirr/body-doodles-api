9649c94dbd6832e4b826452f5654ecbe43c7b70e
Gareth-McGirr <gareth.mcgirr@hotmail.com>
2023-02-28 21:37:58 +0000

    Removed kwargs

--- profiles/models.py
+++ profiles/models.py
@@ -40,7 +40,7 @@ class Profile(models.Model):
         return f"{self.owner}'s profile"
 
 
-def create_profile(sender, instance, created, **kwargs):
+def create_profile(sender, instance, created):
     """Creates profile when a new user is created"""
     if created:
         Profile.objects.create(owner=instance)
